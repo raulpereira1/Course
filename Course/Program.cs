a48fe3ed04ad7dd0d08abd225e4d7633f48002d6
Raulp <Raulp@RAUL>
2023-05-28 18:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            SByte y = 80;
+            int z = x * y;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
